301732eeca41390fd9d479f6d95ab7d48c9c1d04
DESKTOP-6DS6GK9\xristos <matziaris.chrhstos@gmail.com>
2023-10-18 15:25:56 +0300

    Initial commit

---
+++ Week5-Challenge.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int number;
+
+	do
+	{
+		cout << "Please whrite a Possitive Integer: " << endl;
+		cin >> number;
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<int>::max(), '\n');
+			cout << "Invalid number, try again." << endl;
+		}
+		else
+		{
+			if (number <= 0)
+			{
+				cout << "Invalid number, try again." << endl;
+			}
+			else
+			{
+				break;
+			}
+		}
+	} while (true);
+
+	system("cls");
+
+	for (int i = 1; i <= number; i++)
+	{
+		//int dividedNumber = number / i;
+		if (number % i == 0)
+		{
+			//cout << i << " is a value Factor of " << number << endl;
+			if (i + 1 > number)
+			{
+				cout << i << " are the Factor Values of " << number << endl;
+			}
+			else
+			{
+				cout << i << ", ";
+			}
+		}
+	}
+}
+
