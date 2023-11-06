2b9a0361d422b4f0295ccbe63a60bd9d9b07feaa
DESKTOP-6DS6GK9\xristos <matziaris.chrhstos@gmail.com>
2023-11-06 17:17:19 +0200

    Initial Commit

---
+++ Week8-Game/Final_Beginers_Programming_Course_Project.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include "Game.h"
+
+using namespace std;
+
+int main()
+{
+	Game myGame;
+
+	if (myGame.Load())
+	{
+		while (!myGame.IsGameOver())
+		{
+			myGame.Run();
+		}
+
+		cout << "YOU WIN!!!" << endl;
+		//PlayWinSound();
+	}
+	else
+	{
+		cout << "Game did not load Terminating now!" << endl;
+	}
+}
+
+//void playdoorclosedsound()
+//{
+//	beep(500, 75);
+//	beep(500, 75);
+//}
+//void playdooropensound()
+//{
+//	beep(1379, 200);
+//}
+//void playkeypickupsound()
+//{
+//	beep(1568, 200);
+//}
+//void playwinsound()
+//{
+//	beep(1568, 200);
+//	beep(1568, 200);
+//	beep(1568, 200);
+//	beep(1245, 1000);
+//	beep(1397, 200);
+//	beep(1397, 200);
+//	beep(1397, 200);
+//	beep(1175, 1000);
+//}
